70e3c15f68b3cc7e51bcb231fe35b65565f77a8b
BackGwa <backgwa@icloud.com>
2023-04-14 23:36:03 +0900

    학교 찾기 기능 API 구현

--- script/finder.js
+++ script/finder.js
@@ -1,12 +1,30 @@
 
+const APIKey = secret.key;
+const SCNM = '중학교';
+const ATPT = 'N10';
+
 // Find School
 function findschool(){
     const schoolname = document.getElementById('schoolname').value;
     if(schoolname != ''){
+        const APILink = `https://open.neis.go.kr/hub/schoolInfo?Type=json&KEY=${APIKey}&SCHUL_KND_SC_NM=${SCNM}&ATPT_OFCDC_SC_CODE=${ATPT}&SCHUL_NM=${schoolname}`;
+
+        fetch(APILink)
+        .then(
+            (response) => response.json()
+        )
+        .then(
+            (data) => showlist(data)
+        );
 
     }else{
         message('학교 이름을 입력해주세요!');
     }
+}
+
+// Show School list
+function showlist(data){
+    console.log(data);
 }
 
 // ErrorMessage
